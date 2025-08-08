9108bafa7c3d81ced620f2b41dcaf1bb047acf62
gaurav avhale <avhalegaurav07@gmail.com>
2025-08-08 18:19:40 +0530

    env changes

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({ origin:[ 'http://localhost:3000', 'https://wanderlusttravelhub.vercel.app/' ], credentials: true }));
+app.use(cors({ origin:[ 'http://localhost:3000', 'https://wanderlusttravelhub.vercel.app' ], credentials: true }));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
